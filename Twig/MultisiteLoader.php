53c0e021a020344c4ad6c505061374973ca80f88
Sébastien Lévêque <legenyes@gmail.com>
2016-03-27 15:14:09 +0200

    Revert "Replace Twig_LoaderInterface by Twig_Loader_Filesystem to ekkp Twig_Loader_Array intact"
    This reverts commit 6d4da35a00bde20c0fb23f9d1766563cc20b2423.

--- Twig/MultisiteLoader.php
+++ Twig/MultisiteLoader.php
@@ -7,10 +7,10 @@ use Alex\MultisiteBundle\Branding\SiteContext;
 /**
  * Wraps a Twig Loader and extends template syntax
  */
-class MultisiteLoader extends \Twig_Loader_Filesystem
+class MultisiteLoader implements \Twig_LoaderInterface
 {
     /**
-     * @var Twig_Loader_Filesystem
+     * @var Twig_LoaderInterface
      */
     private $loader;
 
